fe3c4614bb0ad38044590bc4f0917263e0d0f54a
Niklas <andreasson.n@gmail.com>
2013-05-12 21:18:39 +0200

    Added some initial code

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,7 +1,35 @@
-;(function() {
+requirejs.config({
+  baseUrl: 'javascripts/lib',
+  paths: {
+    app: '../game'
+  }
+});
+requirejs(['ball', 'paddle'], function(Ball, Paddle) {
+  // animation frame code
+  window.requestAnimFrame = (function() {
+    return  window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+        function( callback ) { window.setTimeout(callback, 1000 / 60 ); }
+  })();
 
-  var init = function() {
-    // start some stuff in here
+  var settings = {
+    bounds: {
+      width: 800
+    , height: 400
+    }
   };
 
-}());
+  // init code
+  var leftPaddle = new Paddle()
+    , rightPaddle = new Paddle()
+    , ball = new Ball();
+
+  // draw
+  //  draw background
+  //  draw left paddle
+  //  draw right paddle
+  //  draw ball
+});
