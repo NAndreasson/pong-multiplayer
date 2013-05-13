f86b9ac1f8207ce1313551c2d2ed6350db5c2d9d
Niklas Andréasson <eaglus_@hotmail.com>
2013-05-13 17:34:07 +0200

    Started working a bit on a game model

---
+++ public/javascripts/game/gamemodel.js
@@ -0,0 +1,21 @@
+function GameModel(nrOfRounds) {
+  this.currentRoundNr = 1;
+  this.nrOfRounds = nrOfRounds;
+  this.running = false;
+}
+
+GameModel.prototype.start = function() {
+  this.running = true;
+};
+
+GameModel.prototype.pause = function() {
+  this.running = false;
+};
+
+GameModel.prototype.update = function(dt) {
+  // handle player input
+
+  // update ball
+};
+
+return GameModel;
